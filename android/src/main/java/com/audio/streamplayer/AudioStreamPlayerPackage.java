ef3fac09a2ffe3dac9cffa39d2c28ef847a4b132
ngoc.nguyen07 <ngocnguyen07@Mac-mini-cua-ngocnguyen07.local>
2025-06-06 18:22:15 +0700

    Update android

--- android/src/main/java/com/audio/streamplayer/AudioStreamPlayerPackage.java
+++ android/src/main/java/com/audio/streamplayer/AudioStreamPlayerPackage.java
@@ -8,10 +8,10 @@ import com.facebook.react.uimanager.ViewManager;
 import java.util.Collections;
 import java.util.List;
 
-public class RNPCMPlayerPackage implements ReactPackage {
+public class AudioStreamPlayerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Collections.singletonList(new RNPCMPlayerModule(reactContext));
+        return Collections.singletonList(new AudioStreamPlayerModule(reactContext));
     }
 
     @Override
